82273a6ef63d09693e3888cb61199fd725cb6a16
ingchrist <cdjinguet@gmail.com>
2024-05-15 18:53:49 +0100

    files added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -48,8 +48,6 @@ const Login = () => {
         <h2>Sign In</h2>
         <p>Please Login To Continue</p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat culpa
-          voluptas expedita itaque ex, totam ad quod error?
         </p>
         <form onSubmit={handleLogin}>
           <input
